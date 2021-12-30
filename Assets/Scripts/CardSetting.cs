461a2b71efe7341f9cc6aefe8929e5f06257647d
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2021-12-30 23:49:52 +0800

    更新美術素材及部分CardSetting。更新卡片樣式。

---
+++ Assets/Scripts/CardSetting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card Setting", menuName = "Card/Create Card Setting", order = 0)]
+public class CardSetting : ScriptableObject
+{
+    public string imageName;
+    public string author;
+    [TextArea(3, 10)]
+    public string description;
+    public Sprite image;
+}
